9ea7707830c7d887c332ec5d020035be8018bf45
JUAN <juanbeta00@hotmail.com>
2022-03-18 23:52:07 -0400

    commit tree d2835bd795fcf4e3b272ddb01a8a528c70d7f16a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToGuadalupe()
+  sayHelloToLiz()
+  sayHelloToSamip()
